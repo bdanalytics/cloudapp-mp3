a89d21e57c65bcd35de72aa35aa8783efd7994e3
bdanalytics <bbalaji8@gmail.com>
2015-09-15 14:55:45 +0000

    Submit TopNFinderBolt.java; Keep only Top N words in TreeSet & HashMap ???

--- src/TopNFinderBolt.java
+++ src/TopNFinderBolt.java
@@ -6,12 +6,15 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 import java.util.HashMap;
+import java.util.TreeSet;
+import java.util.Iterator;
 
 /**
  * a bolt that finds the top n words.
  */
 public class TopNFinderBolt extends BaseBasicBolt {
   private HashMap<String, Integer> currentTopWords = new HashMap<String, Integer>();
+  private TreeSet<Pair<Integer, String>> currentCountWordTupleSet = new TreeSet<Pair<Integer, String>>();
   private int N;
 
   private long intervalToReport = 20;
@@ -29,7 +32,17 @@ public class TopNFinderBolt extends BaseBasicBolt {
 
 
     ------------------------------------------------- */
+    String word = tuple.getString(0);
+    Integer tupleCount = tuple.getInteger(1);	
+    Integer hashCount = currentTopWords.get(word);
 
+    if (hashCount != null)
+	currentCountWordTupleSet.remove(new Pair<Integer, String>(hashCount, word));
+
+    currentTopWords.put(word, (hashCount == null) ? tupleCount : hashCount + tupleCount);
+
+    // Sort the words by count & word
+    currentCountWordTupleSet.add(new Pair<Integer, String>(currentTopWords.get(word), word));	
 
     //reports the top N words periodically
     if (System.currentTimeMillis() - lastReportTime >= intervalToReport) {
@@ -48,9 +61,19 @@ public class TopNFinderBolt extends BaseBasicBolt {
   public String printMap() {
     StringBuilder stringBuilder = new StringBuilder();
     stringBuilder.append("top-words = [ ");
-    for (String word : currentTopWords.keySet()) {
-      stringBuilder.append("(" + word + " , " + currentTopWords.get(word) + ") , ");
+
+    //for (String word : currentTopWords.keySet()) {
+    //  stringBuilder.append("(" + word + " , " + currentTopWords.get(word) + ") , ");
+    //}
+
+    Iterator<Pair<Integer, String>> dItr = currentCountWordTupleSet.descendingIterator();
+    Integer printN = 0;
+    while ((printN < N) && dItr.hasNext()) {
+      Pair<Integer, String> countWordTuple = dItr.next();
+      stringBuilder.append("(" + countWordTuple.second + " , " + countWordTuple.first + ") , ");
+      printN++;
     }
+
     int lastCommaIndex = stringBuilder.lastIndexOf(",");
     stringBuilder.deleteCharAt(lastCommaIndex + 1);
     stringBuilder.deleteCharAt(lastCommaIndex);
@@ -59,3 +82,57 @@ public class TopNFinderBolt extends BaseBasicBolt {
 
   }
 }
+
+class Pair<A extends Comparable<? super A>,
+        B extends Comparable<? super B>>
+        implements Comparable<Pair<A, B>> {
+
+    public final A first;
+    public final B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <A extends Comparable<? super A>,
+            B extends Comparable<? super B>>
+    Pair<A, B> of(A first, B second) {
+        return new Pair<A, B>(first, second);
+    }
+
+    @Override
+    public int compareTo(Pair<A, B> o) {
+        int cmp = o == null ? 1 : (this.first).compareTo(o.first);
+        return cmp == 0 ? (this.second).compareTo(o.second) : cmp;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * hashcode(first) + hashcode(second);
+    }
+
+    private static int hashcode(Object o) {
+        return o == null ? 0 : o.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Pair))
+            return false;
+        if (this == obj)
+            return true;
+        return equal(first, ((Pair<?, ?>) obj).first)
+                && equal(second, ((Pair<?, ?>) obj).second);
+    }
+
+    private boolean equal(Object o1, Object o2) {
+        return o1 == o2 || (o1 != null && o1.equals(o2));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ')';
+    }
+}
+
